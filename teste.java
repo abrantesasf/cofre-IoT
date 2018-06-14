f841642ddf09d68e3db98991af92649a0a31c206
Abrantes Araújo Silva Filho <abrantesasf@gmail.com>
2018-06-13 21:29:19 -0300

    Add files via upload
    Comando de limpeza do console

---
+++ teste.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class teste{
+
+public static void main (String[] args) {
+System.out.println("teste");
+
+try {
+  if( System.getProperty( "os.name" ).startsWith( "Window" ) )
+    {
+     Runtime.getRuntime().exec("cls");
+
+    }
+    else {
+     Runtime.getRuntime().exec("clear");
+  }
+    
+
+
+} 
+    catch (Exception e) {
+ 
+
+  for(int i = 0; i < 1000; i++) {
+
+    System.out.println();
+  }
+
+
+}
+
+
+}
+}
